a7b8e5ac8d8d0271ef6b476ea90a183f5dbf65ce
ashhar-sartaj <ashharsartaj04@gmail.com>
2026-01-17 15:19:45 +0530

    backend index file cleaned

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -3,17 +3,9 @@ import app from "./app.js";
 import http from 'http';
 import { Server } from "socket.io";
 import { initialiseSocketIO } from "./SocketServer/server.js";
-/*
-const httpServer = http.createServer(app); //wrapping express app in http server
-const io = new Server(httpServer, {
-    cors: {
-        origin: 'http://localhost:5173',
-        methods: ['GET', 'POST', 'PUT', 'DELETE']
-    }
-}); //io is an instance of Socket.io Server class. thus, your Socket.io server attached to your http server.
-*/
-const httpServer = http.createServer(app);
-const io = new Server(httpServer); //here, http socket request gets upgrades to ws request
+
+const httpServer = http.createServer(app);//wrapping express app in http server
+const io = new Server(httpServer); //here, http socket request gets upgrades to ws request. //io is an instance of Socket.io Server class. thus, your Socket.io server attached to your http server.
 initialiseSocketIO(io); 
 const port = 5000;
 httpServer.listen(port, () => {
