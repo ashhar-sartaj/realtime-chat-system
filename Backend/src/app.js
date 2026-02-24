da71dad9ce77e285e17300de7a012de3a10a77b4
ashhar-sartaj <ashharsartaj04@gmail.com>
2026-02-24 12:40:30 +0530

    modified Backend/src/app.js, Backend/src/index.js to fix cors for http and ws requests

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,11 +6,19 @@ import cors from 'cors';
 const app = express();
 app.use(express.json()); //use this middleware only once for .entire app.. no need to defined in individual pages/files(that requires body content to be parse into json). 
 app.use(cors({
-  origin: [
-    'http://localhost:5173',
-    // process.env.FRONTEND_URL //in place of this use: regeex to aviod cors
-    /\.netlify\.app$/
-  ],
+  origin: function (origin, callback) {
+    // allow requests with no origin (like mobile apps, Postman)
+    if (!origin) return callback(null, true);
+
+    if (
+      origin === "http://localhost:5173" ||
+      origin.endsWith(".netlify.app")
+    ) {
+      return callback(null, true);
+    }
+
+    return callback(new Error("Not allowed by CORS"));
+  },
   credentials: true,
 }));
 app.use('/api/auth',logRegRoutes);
