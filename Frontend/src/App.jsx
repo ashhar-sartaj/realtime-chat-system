e1ded63035ae8858d87e5389124b715848667a3e
ashhar-sartaj <ashharsartaj04@gmail.com>
2026-02-24 12:05:41 +0530

    removed redundant chatt.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,8 +2,8 @@ import { useState, useEffect } from 'react'
 import {Routes, Route, Navigate} from 'react-router-dom';
 import Register from './pages/register';
 import Login from './pages/login';
-// import { Chat } from './pages/chat.jsx';
-import Chatt from './pages/chatt.jsx'
+import Chat from './pages/chat.jsx';
+// import Chatt from './pages/chatt.jsx'
 
 function App() {
   const [authenticated, setAuthenticated] = useState(false);
@@ -20,8 +20,8 @@ function App() {
       <Routes>
         <Route path='/register' element={!registerDone ? (<Register onRegister={()=>setRegisterDone(true)}/>) : (<Navigate to='/login' replace/>)}/>
         <Route path='/login' element={!authenticated ? (<Login onLogin={() => setAuthenticated(true)} onRegister={()=>setRegisterDone(false)}/>) : (<Navigate to='/chat' replace/>)}/>
-        {/* <Route path='/chat' element={authenticated ? (<Chat onLogout={()=>setAuthenticated(false)}/>) : (<Navigate to='/login' replace/>)}/> */}
-        <Route path='/chat' element={authenticated ? (<Chatt onLogout={() => setAuthenticated(false)} />) : (<Navigate to='/login' replace />)} />
+        <Route path='/chat' element={authenticated ? (<Chat onLogout={()=>setAuthenticated(false)}/>) : (<Navigate to='/login' replace/>)}/>
+        {/* <Route path='/chat' element={authenticated ? (<Chatt onLogout={() => setAuthenticated(false)} />) : (<Navigate to='/login' replace />)} /> */}
         <Route path='/' element={<Navigate to='/register' replace/>}/>
       </Routes>
     </div>
