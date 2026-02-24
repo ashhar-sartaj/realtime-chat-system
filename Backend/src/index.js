8695eeb5ef6f8d1018517c07d7de814b1789cd82
ashhar-sartaj <ashharsartaj04@gmail.com>
2026-02-24 12:47:06 +0530

    cors issue in Backend/src/index.js

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -9,12 +9,20 @@ const httpServer = http.createServer(app);//wrapping express app in http server
 //here, http socket request gets upgrades to ws request. //io is an instance of Socket.io Server class. thus, your Socket.io server attached to your http server.
 const io = new Server(httpServer, {
     cors: {
-        origin:[
-            'http://localhost:5173',
-            // process.env.FRONTEND_URL
-            /\.netlify\.app$/
-        ],
-        credentials: true,
+        origin: (origin, callback) => {
+
+            if (!origin) return callback(null, true);
+
+            if (
+                origin === "http://localhost:5173" ||
+                origin.endsWith(".netlify.app")
+            ) {
+                return callback(null, true);
+            }
+
+            callback(new Error("Not allowed by CORS"));
+        },
+        credentials: true
     }
 });
 initialiseSocketIO(io); 
